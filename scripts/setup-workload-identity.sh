f4117fac5544f9786c5a7f7a73c26c27cd78b1cc
hebersilva <heber.silva@delta.com>
2024-06-07 12:12:46 -0400

    - auth

--- scripts/setup-workload-identity.sh
+++ scripts/setup-workload-identity.sh
@@ -11,28 +11,28 @@ ROLE="roles/storage.admin"  # Example role, change according to your needs
 
 ## Create GKE cluster with Workload Identity enabled
 #echo "Creating GKE cluster with Workload Identity..."
-#gcloud container clusters create $CLUSTER_NAME \
+#gcloud container clusters create-auto $CLUSTER_NAME \
 #  --zone $ZONE \
-#  --workload-pool=wipro-425519.svc.id.goog \
-#  --project $PROJECT_ID
-#
-## Create a Google Service Account
-#echo "Creating Google Service Account..."
-#gcloud iam service-accounts create $GSA_NAME \
-#  --description="Service account for Kubernetes workload" \
-#  --display-name=$GSA_NAME \
-#  --project $PROJECT_ID
-#
-## Assign roles to the Google Service Account
-#echo "Assigning roles to Google Service Account..."
-#gcloud projects add-iam-policy-binding $PROJECT_ID \
-#  --member="serviceAccount:$GSA_NAME@$PROJECT_ID.iam.gserviceaccount.com" \
-#  --role=$ROLE \
+#  --workload-pool="wipro-425519.svc.id.goog" \
 #  --project $PROJECT_ID
 
+## Create a Google Service Account
+echo "Creating Google Service Account..."
+gcloud iam service-accounts create $GSA_NAME \
+  --description="Service account for Kubernetes workload" \
+  --display-name=$GSA_NAME \
+  --project $PROJECT_ID
+
+## Assign roles to the Google Service Account
+echo "Assigning roles to Google Service Account..."
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:$GSA_NAME@$PROJECT_ID.iam.gserviceaccount.com" \
+  --role=$ROLE \
+  --project $PROJECT_ID
+
 # Create a Kubernetes Service Account in the specified namespace
-#echo "Creating Kubernetes Service Account..."
-#kubectl create serviceaccount $KSA_NAME --namespace $NAMESPACE
+echo "Creating Kubernetes Service Account..."
+kubectl create serviceaccount $KSA_NAME --namespace $NAMESPACE
 
 # Bind the Kubernetes Service Account to the Google Service Account
 echo "Binding Kubernetes Service Account to Google Service Account..."
